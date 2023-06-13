34b356e7788729fbbc5cf6bd87013d2b9f050867
Vladimir <ostapenkov74@gmail.com>
2023-06-13 16:13:49 +0500

    create sign up vue component

--- vue-frontend/src/components/views/Signup.vue
+++ vue-frontend/src/components/views/Signup.vue
@@ -1,11 +1,61 @@
 <script setup>
+  import { ref } from 'vue'
+  const name =ref ('')
+  const email =ref ('')
+  const password =ref ('')
+  const password_confirmation =ref ('')
 
-console.log('signup')
+  const SignUpURL = 'http://localhost:3000/api/v1/sign_up'
+  const createUser = async() =>{
+    const res = await fetch(SignUpURL, {
+      method : 'POST',
+      headers : {'Content-Type' : 'application/json'},
+      body : JSON.stringify({user:{
+        name: name.value,
+        email: email.value,
+        password: password.value,
+        password_confirmation: password_confirmation.value,
+      }})
+  })
+  if (res.status === 201) {
+    name.value = ''
+    email.value = ''
+    password.value = ''
+    password_confirmation.value = ''
+  }
+  }
 </script>
 <template>
-  <h3>Sign up</h3>
+  <h3 class="title">Sign up</h3>
+  <div class="input">
+    <input type="text" v-model="name" placeholder="name" class="input-text"><br>
+    <input type="text" v-model="email" placeholder="email" class="input-text"><br>
+    <input type="text" v-model="password" placeholder="password" class="input-text"><br>
+    <input type="text" v-model="password_confirmation" placeholder="password confirmation" class="input-text"><br>
+    <button @click="createUser" class="btn">Create</button>
+  </div>
 </template>
 
 <style scoped>
+.input {
+  text-align: center;
+}
 
+.input-text{
+  margin-top: 10px;
+  border-radius: 10px;
+  border: 1px solid #3906f1;
+}
+
+.btn{
+  background-color: rgb(45, 235, 7);
+  color: blue;
+  margin-top: 10px;
+  border-radius: 10px;
+}
+
+.title{
+  text-align: center;
+  color: #07a3eb;
+}
 </style>
